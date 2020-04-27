dd9b1c8a22c02c1f880bbb6e37b9415370832d57
mpivoney <mpivoney@yahoo.com>
2020-04-26 21:20:14 -0500

    LS Talks

--- ialu_app/lib/main.dart
+++ ialu_app/lib/main.dart
@@ -1,24 +1,80 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
 
+import 'package:ialu_app/screens/home_screen.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatelessWidget
+{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello  World'),
-        ),
-      ),
+    return new MaterialApp(
+       home: new FirstScreen(),
     );
   }
 }
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+     return new Scaffold(
+        appBar: AppBar(
+          title: Text('IALU Home'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            child: Text('LS Talks'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                 MaterialPageRoute(builder: (context) => SecondScreen()),
+              );
+            },
+          ),
+        ),
+      );
+  }
+}
+
+
+/*
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+      return new Scaffold(
+        appBar: AppBar(
+          title: Text("Second Screen"),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go back!'),
+          ),
+        ),
+    );
+  }
+}
+*/
+
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'La Salle Talks',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
---
+++ ialu_app/lib/utilities/keys.dart
@@ -0,0 +1,1 @@
+const String API_KEY = 'AIzaSyAcjqcDqBklMX6wMKjGLCYVYLDQmuUT29E';
